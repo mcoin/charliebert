1b04f786472a926cb282029a3cdf00f9226ba4ca
mcoin <thomas.vettorel@gmail.com>
2017-09-21 19:14:48 +0200

    Generalized switches

--- charliebert/userInterfaceTest.py
+++ charliebert/userInterfaceTest.py
@@ -3,8 +3,10 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)  
 
 # Switches 
-GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
-GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
+#GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
+switches = { 20: "Switch 1", 21: "Switch 2"}
+for s in switches:
+    GPIO.setup(s, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
   
 # LEDs
 GPIO.setup(12, GPIO.OUT)
@@ -14,10 +16,11 @@ GPIO.output(16, GPIO.HIGH)
 
 # Callback for switches 
 def callbackSwitch(channel):  
-    print("Edge detected on channel {:d}".format(channel))
+    print("Edge detected on channel {:d} [Switch ID: {}]".format(channel, switches[channel]))
     
-GPIO.add_event_detect(20, GPIO.FALLING, callback=callbackSwitch, bouncetime=300)  
-GPIO.add_event_detect(21, GPIO.FALLING, callback=callbackSwitch, bouncetime=300)  
+#GPIO.add_event_detect(20, GPIO.FALLING, callback=callbackSwitch, bouncetime=300)  
+for s in switches:
+    GPIO.add_event_detect(s, GPIO.FALLING, callback=callbackSwitch, bouncetime=300)  
   
 try:  
     print("Reacting to interrupts from switches")  
