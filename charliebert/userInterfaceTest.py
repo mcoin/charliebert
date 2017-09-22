920c71af0d663a696eab8eee063f5afc0fa6e429
mcoin <thomas.vettorel@gmail.com>
2017-09-22 11:32:52 +0200

    Corrected switch/led order

--- charliebert/userInterfaceTest.py
+++ charliebert/userInterfaceTest.py
@@ -6,23 +6,23 @@ GPIO.setmode(GPIO.BCM)
 # Switches 
 #GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
 switches = { 
-            14: "Switch 1", 
-            15: "Switch 2", 
-            18: "Switch 3", 
-            23: "Switch 4", 
-            24: "Switch 5", 
-            25: "Switch 6", 
-            8: "Switch 7", 
-            7: "Switch 8", 
-            12: "Switch 9", 
-            16: "Switch 10", 
-            20: "Switch 11", 
-            21: "Switch 12", 
+            14: "Switch 1",
+            15: "Switch 4",
+            18: "Switch 7",
+            23: "Switch 10",
+            24: "Switch 2",
+            25: "Switch 5",
+            8: "Switch 8",
+            7: "Switch 11",
+            12: "Switch 3",
+            16: "Switch 6",
+            20: "Switch 9",
+            21: "Switch 12",
             5: "Play/Pause", 
-            11: "Back", 
-            9: "Forward", 
-            17: "Mode", 
-            27: "Bank"
+            11: "Forward", 
+            9: "Back", 
+            17: "Bank", 
+            27: "Mode"
             }
 bankSwitch = "Bank"
 assert(bankSwitch in switches.values())
@@ -32,7 +32,7 @@ for s in switches:
 # LEDs
 #GPIO.setup(12, GPIO.OUT)
 #GPIO.output(12, GPIO.HIGH)
-ledPorts = [6, 13, 19, 26]
+ledPorts = [6, 19, 13, 26]
 ledNames = ["Bank A", "Bank B", "Bank C", "Bank D"]
 ledStates = [GPIO.HIGH, GPIO.LOW]
 assert(len(ledPorts) == len(ledNames))
