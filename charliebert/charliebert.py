4521e53e9c073d4ca1b1075e1a54620d590bf586
Thomas Vettorel <thomas.vettorel@gmail.com>
2017-09-28 22:14:07 +0200

    Corrections

--- charliebert/charliebert.py
+++ charliebert/charliebert.py
@@ -4,6 +4,7 @@ import Queue
 import logging
 import re
 from userInterface import UserInterface
+from sonosInterface import SonosInterface
 
 
 class UserInterfaceThread(threading.Thread):
@@ -24,7 +25,9 @@ class SonosInterfaceThread(threading.Thread):
         self.stopper = stopper
         self.queue = queue
         self.room = "Office"
+        #self.room = "Bedroom"
         self.playlistBasename = "zCharliebert"
+        self.si = SonosInterface()
         
     def run(self):
         logging.debug("SonosInterfaceThread starting")
@@ -41,27 +44,27 @@ class SonosInterfaceThread(threading.Thread):
                 # Process commands
                 if re.search("^PLAY/PAUSE$", command):
                     logging.debug("Command PLAY/PAUSE")
-                    si.togglePlayPause(self.room)
+                    self.si.togglePlayPause(self.room)
                 elif re.search("^FORWARD$", command):
                     logging.debug("Command FORWARD")
-                    si.skipToNext(self.room)
+                    self.si.skipToNext(self.room)
                 elif re.search("^BACK$", command):
                     logging.debug("Command BACK")
-                    si.skipToPrevious(self.room)
+                    self.si.skipToPrevious(self.room)
                 elif re.search("^PLAYLIST\s+([A-Z])\s+([0-9]+)\s*$", command):
                     m = re.search("^PLAYLIST\s+([A-Z])\s+([0-9]+)\s*$", command)
-                    logging.debug("Command PLAYLIST: {} {:d}".format(m.group(1, 2)))
-                    playlistName = "{}_{}{0:02d}".format(self.playlistBasename, m.group(1), m.group(2))
+                    logging.debug("Command PLAYLIST: {} {:d}".format(m.group(1), int(m.group(2))))
+                    playlistName = "{0}_{1}{2:02d}".format(self.playlistBasename, m.group(1), int(m.group(2)))
                     logging.debug("Starting playlist {}".format(playlistName))
-                    si.startPlaylist(playlistName, self.room)
+                    self.si.startPlaylist(playlistName, self.room)
                 elif re.search("^TRACK\s+([0-9]+)\s*$", command):
                     m = re.search("^TRACK\s+([0-9]+)\s*$", command)
-                    logging.debug("Command TRACK: {:d}".format(m.group(1)))
-                    si.playTrackNb(m.group(1), self.room)
-                elif re.search("^VOLUME\s+([0-9]+)\s*$", command):
-                    m = re.search("^VOLUME\s+([0-9]+)\s*$", command)
-                    logging.debug("Command VOLUME: {:d}".format(m.group(1)))
-                    si.adjustVolume(m.group(1), self.room)
+                    logging.debug("Command TRACK: {:d}".format(int(m.group(1))))
+                    self.si.playTrackNb(m.group(1), self.room)
+                elif re.search("^VOLUME\s+(-[0-9]+)\s*$", command):
+                    m = re.search("^VOLUME\s+([-0-9]+)\s*$", command)
+                    logging.debug("Command VOLUME: {:d}".format(int(m.group(1))))
+                    self.si.adjustVolume(int(m.group(1)), self.room)
                 else:
                     logging.error("Unrecognized command: '{}'".format(command))
 
